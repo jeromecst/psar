99ce1999b373dfc58190b502d19a3508be86daf3
Jérôme <jerome@jeromecst.com>
2022-04-19 18:35:35 +0200

    using nodes

--- test2.cpp
+++ test2.cpp
@@ -1,9 +1,6 @@
 #include <benchmark/benchmark.h>
 #include "test.h"
 
-/* buffer size is 256KiB */
-#define SIZE 262144
-
 static void BM_PageCache(benchmark::State& state)
 {
 	int fd = open("/proc/sys/vm/drop_caches", O_WRONLY);
@@ -14,14 +11,24 @@ static void BM_PageCache(benchmark::State& state)
 	close(fd);
 
 	/* only one read will fill the page cache */
-	/* read on CPU X */
-	setaffinity(state.range(0));
-	read_file(nullptr, SIZE);
+	/* read on CPU X (only NUMA node 0) */
+	setaffinity_node(state.range(0));
+	read_file(nullptr);
 
-	setaffinity_any();
-	for (auto _ : state) {
-		read_file(nullptr, SIZE);
+	/* allocate and fill a buffer on CPU Y */
+	setaffinity_node(state.range(1));
+	char *buf = new char[SIZE];
+	memset(buf, 0, SIZE);
+	for (int i = 0; i < SIZE; i++) {
+		buf[i] = i;
 	}
+	
+	setaffinity_any();
+
+	for (auto _ : state) {
+		read_file(buf);
+	}
+	delete[] buf;
 }
 
 BENCHMARK(BM_PageCache)
