4f27521ba94f988106df9db8082f4bcc08e53829
Léo Lam <leo@leolam.fr>
2022-04-21 01:09:59 +0200

    Revert "run more iterations of distant_buffer"
    This reverts commit 848d955d08a556134794939e944e6545df7749be.

--- src/test_distant_reads_distant_buffer.cpp
+++ src/test_distant_reads_distant_buffer.cpp
@@ -4,7 +4,6 @@ int main() {
   constexpr psar::BenchmarkReadsSimpleConfig config{
       .set_affinity_any = true,
       .buffer_location = psar::BufferLocation::OnInitNode,
-      .num_iterations = 10000,
   };
   psar::benchmark_reads_simple<config>(
       "results/test_distant_reads_distant_buffer.json");
