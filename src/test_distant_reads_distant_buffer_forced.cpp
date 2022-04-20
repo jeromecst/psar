2b4681550f21ff34729766841aed6b335b52bab0
Léo Lam <leo@leolam.fr>
2022-04-20 22:31:55 +0200

    add test_distant_reads_local_buffer

--- src/test_distant_reads_distant_buffer_forced.cpp
+++ src/test_distant_reads_distant_buffer_forced.cpp
@@ -3,6 +3,7 @@
 int main() {
   constexpr psar::BenchmarkReadsSimpleConfig config{
       .set_affinity_any = false,
+      .buffer_location = psar::BufferLocation::OnInitNode,
   };
   psar::benchmark_reads_simple<config>(
       "results/test_distant_reads_distant_buffer_forced.json");
