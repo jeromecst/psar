4c88dd7c1c8ff57f1ea1d87ca49388309d06e7c6
Léo Lam <leo@leolam.fr>
2022-04-20 22:23:31 +0200

    rename tests for more clarity

--- src/test_distant_reads_distant_buffer_forced.cpp
+++ src/test_distant_reads_distant_buffer_forced.cpp
@@ -4,5 +4,6 @@ int main() {
   constexpr psar::BenchmarkReadsSimpleConfig config{
       .set_affinity_any = false,
   };
-  psar::benchmark_reads_simple<config>("results/test1.json");
+  psar::benchmark_reads_simple<config>(
+      "results/test_distant_reads_distant_buffer_forced.json");
 }
