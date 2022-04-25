735e85d6d56abb83fb861f1e24e690668013d2e1
Jérôme <jerome@jeromecst.com>
2022-04-25 12:35:24 +0200

    added violin plot

--- barplot.py
+++ barplot.py
@@ -30,17 +30,25 @@ def format_dic(dic, layout_times, warmup):
         for l in layout:
             exp[l] = np.count_nonzero([x == l for x in list_layout]) / len(exp["times_us"])
         # delete useless fields
-        for field in ["times_us","nodes","buffer_core","init_core","read_core"]:
+        for field in ["nodes","buffer_core","init_core","read_core"]:
             exp.pop(field, None)
 
-def plot_bar(df, ax):
+def barplot(df, ax):
     x = range(len(df))
     colors = ['#33cc33', '#3366ff', '#006699', '#DB4444']
     bottom_sum = np.zeros(len(df))
     for i, tag in enumerate(layout):
         ax.bar(x, df[tag], bottom=bottom_sum, color=colors[i])
         bottom_sum += df[tag]
+    ax_read.legend(layout)
 
+def violinplot(df, ax):
+    x = range(len(df))
+    colors = ['#33cc33', '#3366ff', '#006699', '#DB4444']
+    bottom_sum = np.zeros(len(df))
+    for index, row in df.iterrows():
+        df["times_us"][index] = np.array(row["times_us"])[np.array(row["times_us"]) < 25000]
+    ax.violinplot(df["times_us"], np.array(x))
 
 def json_plot_gettime_all(json_dic, name, warmup):
     layout_times = get_layout_times()
@@ -51,17 +59,18 @@ def json_plot_gettime_all(json_dic, name, warmup):
 
     # pandas.set_option("display.max_rows", 65, "display.max_columns", None)
     # print(df)
+
     
     fig, ax = plt.subplots(4, 1, figsize=(14, 20))
 
     xlabels = [ f"({i}, {j})" for i in range(4) for j in range(4)]
     xticks = np.arange(16) 
     for node_read, ax_read in enumerate(np.array(ax).flat):
-        plot_bar(df[node_read::4], ax_read)
+        # barplot(df[node_read::4], ax_read)
+        violinplot(df[node_read::4], ax_read)
         ax_read.set_title(f"read on node {node_read}")
         ax_read.set_xticks(xticks, xlabels)
         ax_read.set_xlabel("(pagecache node, buff node)")
-        ax_read.legend(layout)
 
     fig.savefig("plot/yeti/" + name + ".png")
 
