afcdc0aa7b858259db3016b28bf815d7911b9065
Léo Lam <leo@leolam.fr>
2022-04-27 16:02:48 +0200

    run 2000 iterations of get_time_all

--- src/test_get_time_all_scenarios.cpp
+++ src/test_get_time_all_scenarios.cpp
@@ -3,7 +3,7 @@
 int main() {
 	psar::BenchmarkGetTimesAllConfig config{
 		.allow_migrations_during_reads = true,
-		.num_iterations = 5000,
+		.num_iterations = 2000,
 	};
 	psar::benchmark_reads_get_times_all_scenarios(
 		config, "results/test_get_time_all_scenarios.json");
