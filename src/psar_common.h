0e90a21a0a4b922f488888ccbda18029b15e5f65
Léo Lam <leo@leolam.fr>
2022-05-21 01:45:04 +0200

    add comments

--- src/psar_common.h
+++ src/psar_common.h
@@ -10,12 +10,25 @@
 
 namespace psar {
 
+/// A dynamically allocated contiguous buffer that is suitable for
+/// NUMA experiments.
 struct NumaBuffer {
+	/// Allocates a buffer of `size` bytes using malloc.
+	/// The buffer is guaranteed to be allocated in a fresh page.
 	explicit NumaBuffer(size_t size);
+
+	/// Allocates a buffer of `size` bytes on the specified node
+	/// using numa_alloc_onnode.
+	/// The buffer is guaranteed to be allocated in a fresh page.
 	NumaBuffer(unsigned int node, size_t size);
+
+	/// Frees the buffer.
 	~NumaBuffer();
+
+	/// Copies are not allowed.
 	NumaBuffer(const NumaBuffer &) = delete;
 	auto operator=(const NumaBuffer &) = delete;
+
 	NumaBuffer(NumaBuffer &&other) noexcept {
 		buffer = std::exchange(other.buffer, {});
 		deleter = other.deleter;
@@ -35,22 +48,42 @@ struct NumaBuffer {
 	void (*deleter)(NumaBuffer *buffer){};
 };
 
+/// Drop the page cache. Requires root permissions.
 void drop_caches();
 
+/// Make a buffer that is as large as the test file
+/// and bind it to the specified node (so that it cannot be migrated).
 NumaBuffer make_node_bound_read_buffer(unsigned int node);
+
+/// Make a buffer that is as large as the test file.
+/// Migrations are allowed.
 NumaBuffer make_local_read_buffer();
 
+/// Read the test file into the specified buffer,
+/// using sequential reads of 0x2000 bytes each.
+/// @param buf_size Buffer size. Must match the file size.
 void read_file(char *buf, size_t buf_size);
+/// Read the test file into the specified buffer,
+/// using random reads (read + random seek) of 0x2000 bytes each.
+/// @param buf_size Buffer size. Must match the file size.
 void read_file_random(char *buf, size_t buf_size);
 
+/// Pin the current thread to the specified core.
 void setaffinity(unsigned int core);
+/// Pin the current thread to the specified NUMA node.
 void setaffinity_node(unsigned int node);
+/// Unpin the current thread.
 void setaffinity_any();
 
+/// Returns the number of CPU cores.
 unsigned int get_num_cores();
+/// Returns the number of NUMA nodes.
 unsigned int get_num_nodes();
+/// Returns the current NUMA node. Very fast.
 unsigned int get_current_node();
 
+/// Calls the specified function and returns the amount
+/// of real time it took to run (in microseconds).
 template <typename Fn> long time_us(const Fn &fn) {
 	// libstdc++ and libc++ both use clock_gettime(CLOCK_MONOTONIC) on POSIX
 	auto start = std::chrono::steady_clock::now();
@@ -66,26 +99,41 @@ enum class Location {
 };
 
 struct BenchmarkReadsConfig {
+	/// whether the scheduler should be allowed to migrate the thread
 	bool allow_migrations_during_reads = true;
 	/// whether the read buffer should be bound to buffer_core
 	/// (if true, the buffer will never be migrated automatically)
 	bool bind_read_buffer = false;
+
 	/// perform random reads (read random chunks of ReadChunkSize bytes)
 	bool random_reads = false;
-	int pagecache_core = 0;
-	int read_core = 0;
-	int buffer_core = 0;
+	/// number of times the file should be read in its entirety
 	int num_iterations = 5000;
+
+	/// core on which the file should be loaded into the page cache
+	int pagecache_core = 0;
+	/// core on which reads should be done (initially; the thread might migrate
+	/// to another core if migrations are allowed)
+	int read_core = 0;
+	/// core on which the buffer should be allocated (initially; the buffer
+	/// might migrate to another node if it is not bound)
+	int buffer_core = 0;
 };
 
+/// A BenchmarkResult is a series of measurements.
 struct BenchmarkResult {
+	/// A Measurements is a series of data points for a given set of initial
+	/// placement parameters (see BenchmarkReadsConfig).
 	struct Measurements {
+		/// See BenchmarkReadsConfig for more information on the meaning
+		/// of these parameters.
 		unsigned int pagecache_core;
 		unsigned int pagecache_node;
 		unsigned int read_core;
 		unsigned int read_node;
 		unsigned int buffer_core;
 		unsigned int buffer_node;
+
 		/// times in μs
 		std::vector<long> times_us;
 		/// the active node at the end of each sample
@@ -104,9 +152,12 @@ struct BenchmarkResult {
 	std::vector<Measurements> measurements;
 };
 
+/// Execute benchmarks with the specified config.
+/// Measurements are added to `result`.
 void benchmark_reads(const BenchmarkReadsConfig &config,
                      BenchmarkResult &result);
 
+/// See BenchmarkReadsConfig for the meaning of these parameters.
 struct BenchmarkReadsSimpleConfig {
 	bool allow_migrations_during_reads = true;
 	Location buffer_location = Location::OnReadCore;
@@ -117,6 +168,7 @@ struct BenchmarkReadsSimpleConfig {
 void benchmark_reads_simple(const BenchmarkReadsSimpleConfig &config,
                             const std::string &output_file);
 
+/// See BenchmarkReadsConfig for the meaning of these parameters.
 struct BenchmarkGetTimesConfig {
 	bool allow_migrations_during_reads = false;
 	int node_a = 1;
@@ -124,12 +176,19 @@ struct BenchmarkGetTimesConfig {
 	int num_iterations = 5000;
 };
 
+/// Measure read times in various situations (local page cache / local buffer,
+/// local/remote, remote/local, remote/remote) with the specified config.
+/// Measurements are added to `result`.
 void benchmark_reads_get_times(const BenchmarkGetTimesConfig &config,
                                BenchmarkResult &result);
 
+/// Measure read times in various situations (local page cache / local buffer,
+/// local/remote, remote/local, remote/remote) with the specified config.
+/// Measurements are written the specified output file.
 void benchmark_reads_get_times(const BenchmarkGetTimesConfig &config,
                                const std::string &output_file);
 
+/// See BenchmarkReadsConfig for the meaning of these parameters.
 struct BenchmarkGetTimesAllConfig {
 	bool allow_migrations_during_reads = true;
 	bool bind_read_buffer = false;
@@ -137,9 +196,12 @@ struct BenchmarkGetTimesAllConfig {
 	int num_iterations = 5000;
 };
 
+/// Measure read times and locality information with the specified config.
+/// Measurements are written the specified output file.
 void benchmark_reads_get_times_all_scenarios(
 	const BenchmarkGetTimesAllConfig &config, const std::string &output_file);
 
+/// Initialises `config` from command line arguments.
 void parse_get_times_all_config(BenchmarkGetTimesAllConfig *config, int argc,
                                 const char **argv);
 
